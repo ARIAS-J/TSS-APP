6b8bed3708822f3f287e5d5266e200f04c3b0dcb
No-Hex <evilheroe1@gmail.com>
2022-05-17 10:54:04 -0400

    Agregar archivos de proyecto.

---
+++ TSS-APP/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TSS_APP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Leer Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Leer_Archivo_btn);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cargar Informacion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Cargar_Informacion_btn);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(201, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(856, 405);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 462);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tesoreria de la Seguridad Social";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ TSS-APP/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace TSS_APP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Leer_Archivo_btn(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Cargar_Informacion_btn(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("");
+
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                ofd.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+                ofd.FilterIndex = 1;
+
+                ofd.ShowDialog();
+
+                string archivo = ofd.ToString();
+
+                if (archivo.Trim().Length > 0)
+                    using (StreamReader sr = new StreamReader(archivo))
+                    {
+
+                        DataTable dt = new DataTable();
+
+
+                        String line = sr.ReadToEnd();
+                        dataGridView1.DataSource = dt;
+                    }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hubo a ocurrido un error: " + ex.Message + ex.StackTrace);
+            }
+
+        }
+    }
+}
